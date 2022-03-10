3bf62296f6ac81cee801a53e20291c07f5d57f8a
CKiumi <c.kiumi.oval17@gmail.com>
2022-03-10 13:28:27 +0900

    rename enum variant name

--- src/binding.rs
+++ src/binding.rs
@@ -2,9 +2,9 @@ include!(concat!(env!("OUT_DIR"), "/qulacs.rs"));
 pub use num::complex::Complex;
 
 pub enum Gate {
-	SingleGate(u32, unsafe extern "C" fn(u32, *mut CTYPE, u64)),
-	ControlledGate(u32, u32, unsafe extern "C" fn(u32, u32, *mut CTYPE, u64)),
-	RotationGate(u32, f64, unsafe extern "C" fn(u32, f64, *mut CTYPE, u64)),
+	Single(u32, unsafe extern "C" fn(u32, *mut CTYPE, u64)),
+	Controlled(u32, u32, unsafe extern "C" fn(u32, u32, *mut CTYPE, u64)),
+	Rotation(u32, f64, unsafe extern "C" fn(u32, f64, *mut CTYPE, u64)),
 }
 
 pub fn wrap(state: &mut [Complex<f64>], gate: Gate) {
@@ -12,13 +12,13 @@ pub fn wrap(state: &mut [Complex<f64>], gate: Gate) {
 	let state_ptr = state.as_mut_ptr() as *mut CTYPE;
 	unsafe {
 		match gate {
-			Gate::SingleGate(target_qubit_index, gate) => {
+			Gate::Single(target_qubit_index, gate) => {
 				gate(target_qubit_index, state_ptr, dim);
 			}
-			Gate::ControlledGate(control_qubit_index, target_qubit_index, gate) => {
+			Gate::Controlled(control_qubit_index, target_qubit_index, gate) => {
 				gate(control_qubit_index, target_qubit_index, state_ptr, dim);
 			}
-			Gate::RotationGate(target_qubit_index, angle, gate) => {
+			Gate::Rotation(target_qubit_index, angle, gate) => {
 				gate(target_qubit_index, angle, state_ptr, dim);
 			}
 		}
